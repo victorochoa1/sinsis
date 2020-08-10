0139df46c755b63838b4e8493375654c873b0e3b
victor ochoa <tic1301029@academica.utbb.edu.mx>
2020-08-10 09:14:40 -0500

    proyecto final3

--- conexion.php
+++ conexion.php
@@ -2,8 +2,10 @@
 
 /*$conexion=mysqli_connect('localhost','sinsis','root','sinsis');
 $db=new mysqli("localhost","sinsis","root","sinsis");*/
-$conexion=mysqli_connect('localhost','whiqwwlyimlukm','e7a0185d43ddc4c59d96d14fd87165d30e44b37e6583f1bbeb1591d64ef91503','sinsis');
-$db=new mysqli("localhost","whiqwwlyimlukm","e7a0185d43ddc4c59d96d14fd87165d30e44b37e6583f1bbeb1591d64ef91503","sinsis");
+$conexion= new PDO('pgsql:host=ec2-35-173-94-156.compute-1.amazonaws.com;port=5432;dbname=daj11ftgi9ivai','whiqwwlyimlukm','
+e7a0185d43ddc4c59d96d14fd87165d30e44b37e6583f1bbeb1591d64ef91503');
+$db=new PDO('pgsql:ec2-35-173-94-156.compute-1.amazonaws.com;port=5432;dbname=daj11ftgi9ivai','whiqwwlyimlukm','
+e7a0185d43ddc4c59d96d14fd87165d30e44b37e6583f1bbeb1591d64ef91503');
 if(mysqli_connect_errno()){
     echo '<p>Error: no se pudo conectar a la BD.<br/>Por favor intenta nuevamente mas tarde</p>';
     exit;
